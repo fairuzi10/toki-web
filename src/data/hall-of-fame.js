5b819ebb41228d7cd657a38aebd916dc9dc3d3c4
kalEl2001 <samuel92@ui.ac.id>
2020-03-21 10:04:35 +0700

    IOI Hall of Fame data finished

--- src/data/hall-of-fame.js
+++ src/data/hall-of-fame.js
@@ -1,13 +1,15 @@
 export const medalType = {
   GOLD: "GOLD",
   SILVER: "SILVER",
-  BRONZE: "BRONZE"
+  BRONZE: "BRONZE",
+  NONE: "NONE"
 };
 
 export const medalColor = {
   [medalType.GOLD]: "#FFD700",
   [medalType.SILVER]: "#A9A9A9",
-  [medalType.BRONZE]: "#CD7F32"
+  [medalType.BRONZE]: "#CD7F32",
+  [medalType.NONE]: "#FFFFFF00"
 };
 
 export const IOI = [
@@ -118,7 +120,564 @@ export const IOI = [
         school: "SMA Intan Permata Hati Surabaya"
       }
     ]
-  }
+  },
+  {
+    year: 2015,
+    city: "Almaty",
+    country: "Kazakhstan",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Michael Wibawa",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.SILVER,
+        name: "Agus Sentosa Hermawan",
+        school: "SMA Petra 2 Surabaya"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Muhammad Ayaz Dzulfikar",
+        school: "SMA YPVDV Bontang"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Stacia Edina Johanna",
+        school: "SMA Petra 3 Surabaya"
+      }
+    ]
+  },
+  {
+    year: 2014,
+    city: "Taipei",
+    country: "Taiwan",
+    participants: [
+      {
+        medal: medalType.BRONZE,
+        name: "Alfonsus Raditya Arsadjaja",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Muhammad Rais Fathin Mudzakir",
+        school: "SMAN 1 Bogor"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Zamil Majdy",
+        school: "SMA Semesta Semarang"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Stefano Chiesa Suryanto",
+        school: "SMAK 3 BPK PENABUR Jakarta"
+      }
+    ]
+  },
+  {
+    year: 2013,
+    city: "Brisbane",
+    country: "Australia",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Nathan Azaria",
+        school: "SMAN 2 Purwokerto"
+      },
+      {
+        medal: medalType.SILVER,
+        name: "Jonathan Irvin Gunawan",
+        school: "SMAK 1 BPK PENABUR Bandung"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Ammar Fathin Sabili",
+        school: "SMAN 1 Sragen BBS"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Stefano Chiesa Suryanto",
+        school: "SMAK 3 BPK PENABUR Jakarta"
+      }
+    ]
+  },
+  {
+    year: 2012,
+    city: "Sirmione/Montichiari",
+    country: "Italy",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Nathan Azaria",
+        school: "SMAN 2 Purwokerto"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Jonathan Irvin Gunawan",
+        school: "SMAK 1 BPK PENABUR Bandung"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Cakra Wishnu Wardhana",
+        school: "SMAN 8 Yogyakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Muhammad Aji Muharrom",
+        school: "MAN Insan Cendekia Serpong"
+      }
+    ]
+  },
+  {
+    year: 2011,
+    city: "Pattaya",
+    country: "Thailand",
+    participants: [
+      {
+        medal: medalType.BRONZE,
+        name: "Frederikus Hudi",
+        school: "SMAN 4 Denpasar"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "William Gozali",
+        school: "SMA Tarakanita 2 Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Reinhart Abdiel Hermanus",
+        school: "SMAK Paulus Bandung"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Jessica Handojo",
+        school: "SMA Santa Ursula Jakarta"
+      }
+    ]
+  },
+  {
+    year: 2010,
+    city: "Waterloo",
+    country: "Canada",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Alham Fikri Aji",
+        school: "SMAN 1 Depok"
+      },
+      {
+        medal: medalType.SILVER,
+        name: "Ashar Fuadi",
+        school: "SMAN 1 Bogor"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Christianto Handojo",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Harta Wijaya",
+        school: "SMA St. Thomas 1 Medan"
+      },
+    ]
+  },
+  {
+    year: 2009,
+    city: "Plovdiv",
+    country: "Bulgaria",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Angelina Veni Johanna",
+        school: "SMAK 1 BPK PENABUR Jakarta"
+      },
+      {
+        medal: medalType.SILVER,
+        name: "Reinardus Surya Pradhitya",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Risan",
+        school: "SMAN 1 Tangerang"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Christianto Handojo",
+        school: "SMA Kanisius Jakarta"
+      }
+    ]
+  },
+  {
+    year: 2008,
+    city: "Cairo",
+    country: "Egypt",
+    participants: [
+      {
+        medal: medalType.GOLD,
+        name: "Irvan Jahja",
+        school: "SMA St. Aloysius 1 Bandung"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Reinardus Surya Pradhitya",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Risan",
+        school: "SMAN 1 Tangerang"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Listiarso Wastuargo",
+        school: "SMAN 3 Yogyakarta"
+      }
+    ]
+  },
+  {
+    year: 2007,
+    city: "Zagreb",
+    country: "Croatia",
+    participants: [
+      {
+        medal: medalType.BRONZE,
+        name: "Brian Marshal",
+        school: "SMAK 1 BPK PENABUR Bandung"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Ricky Jeremiah Djajadi",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Karol Danutama",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Riza Oktavian Nugroho Suminto",
+        school: "SMA Taruna Nusantara Magelang"
+      }
+    ]
+  },
+  {
+    year: 2006,
+    city: "Merida",
+    country: "Mexico",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Derianto Kusuma",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Kevin Tanadi",
+        school: "SMA Sutomo 1 Medan"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Daniel Aris Pandu Prasetia",
+        school: "SMA IPEKA Puri Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Aditya Kristanto Goenawan",
+        school: "SMA Kanisius Jakarta"
+      }
+    ]
+  },
+  {
+    year: 2005,
+    city: "Nowy Sacz",
+    country: "Poland",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Derianto Kusuma",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.SILVER,
+        name: "Andrian Kurniady",
+        school: "SMAK Sang Timur Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Sonny Budiman Sasaka",
+        school: "SMAK St. Louis 1 Surabaya"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Arief Widhiyasa",
+        school: "SMAN 1 Singaraja"
+      }
+    ]
+  },
+  {
+    year: 2004,
+    city: "Athens",
+    country: "Greece",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Derianto Kusuma",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.SILVER,
+        name: "David Santoso Anggakusuma",
+        school: "SMA Tarakanita 2 Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Andrian Kurniady",
+        school: "SMAK Sang Timur Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Lie Roberto Eliantono Adiseputra",
+        school: "SMAK Santo Yusuf Malang"
+      }
+    ]
+  },
+  {
+    year: 2003,
+    city: "Kenosha",
+    country: "USA (tidak bertanding karena masalah visa)",
+    participants: [
+      {
+        medal: medalType.NONE,
+        name: "Derianto Kusuma",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Budiarto Herman",
+        school: "SMA Regina Pacis Bogor"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Pascal Alfadian Nugroho",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Stephanus Indra",
+        school: "SMAK 1 BPK PENABUR Jakarta"
+      }
+    ]
+  },
+  {
+    year: 2002,
+    city: "Yong-In",
+    country: "South Korea",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Widagdo Setiawan",
+        school: "SMAN 1 Denpasar"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Randy Sugianto",
+        school: "SMA St. Aloysius 1 Bandung"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Ilham Winata Kurnia",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Felix Halim",
+        school: "SMAK 1 BPK PENABUR Jakarta"
+      }
+    ]
+  },
+  {
+    year: 2001,
+    city: "Tampere",
+    country: "Finland",
+    participants: [
+      {
+        medal: medalType.BRONZE,
+        name: "Widagdo Setiawan",
+        school: "SMAN 1 Denpasar"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Ardian Kristanto Poernomo",
+        school: "SMAK St. Louis 1 Surabaya"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Johan Mustika Kartiwa",
+        school: "SMAK 1 BPK PENABUR Bandung"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Hendy Irawan",
+        school: "SMAN 2 Kediri"
+      }
+    ]
+  },
+  {
+    year: 2000,
+    city: "Beijing",
+    country: "People's Republic of China",
+    participants: [
+      {
+        medal: medalType.BRONZE,
+        name: "Johan Mustika Kartiwa",
+        school: "SMAK 1 BPK PENABUR Bandung"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Hendy Irawan",
+        school: "SMAN 2 Kediri"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Widagdo Setiawan",
+        school: "SMAN 1 Denpasar"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Sony Raditya Suryawijaya",
+        school: "SMAN 1 Magelang"
+      }
+    ]
+  },
+  {
+    year: 1999,
+    city: "Antalya-Belek",
+    country: "Turkey",
+    participants: [
+      {
+        medal: medalType.NONE,
+        name: "Anugrah Redja Kusuma",
+        school: "SMAN 8 Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Hendy Irawan",
+        school: "Kediri"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Johan Mustika Kartiwa",
+        school: "SMAK 1 BPK PENABUR Bandung"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Sony Raditya Suryawijaya",
+        school: "SMAN 1 Magelang"
+      }
+    ]
+  },
+  {
+    year: 1998,
+    city: "Setubal",
+    country: "Portugal",
+    participants: [
+      {
+        medal: medalType.SILVER,
+        name: "Andy Kurnia",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.SILVER,
+        name: "Rusmin Sutjipto",
+        school: "SMA Pelita Harapan Tangerang"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Liong Sauw Ming",
+        school: "SMAK Petra 2 Surabaya"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Johan Mustika Kartiwa",
+        school: "SLTPK 5 BPK PENABUR Bandung"
+      }
+    ]
+  },
+  {
+    year: 1997,
+    city: "Cape Town",
+    country: "South Africa",
+    participants: [
+      {
+        medal: medalType.GOLD,
+        name: "Andy Kurnia",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.BRONZE,
+        name: "Abdul Aziz Qohhar Noor",
+        school: "SMAN 8 Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Liong Sauw Ming",
+        school: "SMAK Petra 2 Surabaya"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Fidens Felix Vicentius Hubertson Simanjuntak",
+        school: "SMAN 8 Jakarta"
+      }
+    ]
+  },
+  {
+    year: 1996,
+    city: "Veszprem",
+    country: "Hungary",
+    participants: [
+      {
+        medal: medalType.NONE,
+        name: "Andy Kurnia",
+        school: "SMA Kanisius Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Ferdinand Fadil",
+        school: "SMAK 1 BPK PENABUR Bandung"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Michael Permana",
+        school: "SMAK 1 BPK PENABUR Jakarta"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Yogy Namara",
+        school: "SMA Frateran Surabaya"
+      },
+      {
+        medal: medalType.NONE,
+        name: "Yudhi Widyatama",
+        school: "SMAN 3 Bandung"
+      },
+    ]
+  },
+  {
+    year: 1995,
+    city: "Eindhoven",
+    country: "Netherlands",
+    participants: [
+      {
+        medal: medalType.NONE,
+        name: "Wirawan Purwanto",
+        school: "SMAK St. Albertus Malang"
+      }
+    ]
+  },
 ];
 
 export const APIO = [
