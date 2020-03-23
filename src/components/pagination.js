4a8f5bd3f7bc2672a7d140d7ae5f2c7344ef1ae6
Muhammad Fairuzi Teguh <mfairuzit@gmail.com>
2020-03-23 13:47:52 +0700

    Change pagination style

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
+import "./pagination.scss";
 
 const Pagination = ({ currentPage, numPages, toUrl }) => {
   return (
